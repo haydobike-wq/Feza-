daf2e30f2b0b671d8f16427db9abe7c2e0532308
Yaren Coruh <yarencoruh1@gmail.com>
2026-06-13 17:02:58 +0300

    Use dist output for Vercel deploy

---
+++ build.js
@@ -0,0 +1,24 @@
+﻿const fs = require("fs");
+const path = require("path");
+
+const distDir = path.join(__dirname, "dist");
+
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir);
+}
+
+const filesToCopy = ["index.html", "style.css", "app.js"];
+
+for (const file of filesToCopy) {
+  const src = path.join(__dirname, file);
+  const dest = path.join(distDir, file);
+
+  if (fs.existsSync(src)) {
+    fs.copyFileSync(src, dest);
+    console.log(`Copied ${file} to dist/`);
+  } else {
+    console.warn(`${file} not found, skipping.`);
+  }
+}
+
+console.log("Static build completed.");
